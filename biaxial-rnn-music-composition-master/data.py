564f37ff397bbc748c3770763096a895a0d2a2ed
Trina K. Locklear <31290532+locklearka17@users.noreply.github.com>
2018-06-16 21:38:25 -0400

    Update data.py

--- biaxial-rnn-music-composition-master/data.py
+++ biaxial-rnn-music-composition-master/data.py
@@ -1,6 +1,7 @@
 import itertools
 from midi_to_statematrix import upperBound, lowerBound
 
+
 def startSentinel():
     def noteSentinel(note):
         position = note
@@ -12,12 +13,14 @@ def startSentinel():
         return part_position + part_pitchclass + [0]*66 + [1] 
     return [noteSentinel(note) for note in range(upperBound-lowerBound)]
 
+
 def getOrDefault(l, i, d):
     try:
         return l[i]
     except IndexError:
         return d
 
+    
 def buildContext(state):
     context = [0]*12
     for note, notestate in enumerate(state):
@@ -25,9 +28,11 @@ def buildContext(state):
             pitchclass = (note + lowerBound) % 12
             context[pitchclass] += 1
     return context
-    
+
+
 def buildBeat(time):
     return [2*x-1 for x in [time%2, (time//2)%2, (time//4)%2, (time//8)%2]]
+
 
 def noteInputForm(note, state, context, beat):
     position = note
@@ -42,10 +47,12 @@ def noteInputForm(note, state, context, beat):
 
     return part_position + part_pitchclass + part_prev_vicinity + part_context + beat + [0]
 
+
 def noteStateSingleToInputForm(state,time):
     beat = buildBeat(time)
     context = buildContext(state)
     return [noteInputForm(note, state, context, beat) for note in range(len(state))]
+
 
 def noteStateMatrixToInputForm(statematrix):
     # NOTE: May have to transpose this or transform it in some way to make Theano like it
