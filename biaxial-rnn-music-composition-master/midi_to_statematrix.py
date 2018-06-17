915c20dcf3c63f9cdbf3951d4537816371722abf
Trina K. Locklear <31290532+locklearka17@users.noreply.github.com>
2018-06-16 21:40:58 -0400

    Update midi_to_statematrix.py

--- biaxial-rnn-music-composition-master/midi_to_statematrix.py
+++ biaxial-rnn-music-composition-master/midi_to_statematrix.py
@@ -3,6 +3,7 @@ import midi, numpy
 lowerBound = 24
 upperBound = 102
 
+
 def midiToNoteStateMatrix(midifile):
 
     pattern = midi.read_midifile(midifile)
@@ -17,6 +18,7 @@ def midiToNoteStateMatrix(midifile):
 
     state = [[0,0] for x in range(span)]
     statematrix.append(state)
+    
     while True:
         if time % (pattern.resolution / 4) == (pattern.resolution / 8):
             # Crossed a note boundary. Create a new state, defaulting to holding notes
@@ -30,16 +32,21 @@ def midiToNoteStateMatrix(midifile):
                 pos = posns[i]
 
                 evt = track[pos]
+                
                 if isinstance(evt, midi.NoteEvent):
+                    
                     if (evt.pitch < lowerBound) or (evt.pitch >= upperBound):
                         pass
                         # print "Note {} at time {} out of bounds (ignoring)".format(evt.pitch, time)
                     else:
+                        
                         if isinstance(evt, midi.NoteOffEvent) or evt.velocity == 0:
                             state[evt.pitch-lowerBound] = [0, 0]
                         else:
                             state[evt.pitch-lowerBound] = [1, 1]
+                            
                 elif isinstance(evt, midi.TimeSignatureEvent):
+                    
                     if evt.numerator not in (2, 4):
                         # We don't want to worry about non-4 time signatures. Bail early!
                         # print "Found time signature event {}. Bailing!".format(evt)
@@ -98,4 +105,5 @@ def noteStateMatrixToMidi(statematrix, name="example"):
     eot = midi.EndOfTrackEvent(tick=1)
     track.append(eot)
 
+    
     midi.write_midifile("{}.mid".format(name), pattern)
